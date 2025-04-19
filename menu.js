f741eecc6001631ae59298f5fd61b0f6730e02ca
rozsavarizsolt <144552734+rozsavarizsolt@users.noreply.github.com>
2025-04-19 13:55:16 +0200

    Update menu.js

--- menu.js
+++ menu.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     <ul>
       <li${current === 'index.html' ? ' class="active"' : ''}><a href="index.html">Kezdőlap</a></li>
       <li${current === 'tablazat.html' ? ' class="active"' : ''}><a href="tablazat.html">Táblázat</a></li>
-      <li${current === 'html5.html' ? ' class="active"' : ''}><a href="html5.html">HTML5 API</a></li>
+      <li${current === 'html5.html' ? ' class="active"' : ''}><a href="html5.html">HTML5</a></li>
       <li${current === 'chartjs.html' ? ' class="active"' : ''}><a href="chartjs.html">ChartJS</a></li>
       <li${current === 'ajax.html' ? ' class="active"' : ''}><a href="ajax.html">AJAX</a></li>
       <li${current === 'oojs.html' ? ' class="active"' : ''}><a href="oojs.html">OOJS</a></li>
